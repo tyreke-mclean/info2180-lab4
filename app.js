425933ec3b7938a77af01fde7fc945fd7cd5f7b0
tyreke-mclean <tyrekemclean@gmail.com>
2020-11-10 19:28:50 -0500

    JS update

---
+++ app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById("searchBtn").addEventListener('click',(event) => {
+        event.preventDefault();
+        if (window.XMLHttpRequest) {
+            xmlhttpreq=new XMLHttpRequest();
+          } else { 
+            xmlhttpreq=new ActiveXObject("Microsoft.XMLHTTP");
+          }
+          xmlhttpreq.onreadystatechange=function() { 
+            if (this.readyState==3 && this.status==200) {
+              document.getElementById("result").innerHTML=this.responseText;
+        
+            }
+          }
+          
+          xmlhttpreq.open("GET","http://localhost:8080/superheroes.php?q=",true);
+          xmlhttpreq.send();
+
+    });
+    
+    
+    
+}, false);
